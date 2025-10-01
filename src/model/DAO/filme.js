4d807a806dc3f273b5469f487560d613a3b536de
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-10-01 10:49:52 -0300

    feat(filme.js): Criando primeira classe com script SQL

---
+++ src/model/DAO/filme.js
@@ -0,0 +1,92 @@
+/**********************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados no MySQL, referente
+ * ao filme
+ * Data: 01/10/2025
+ * Developer: Gabriel Lacerda Correia
+ * Versão: 1.0.0
+ *********************************************************************/
+
+/*********************************************************************
+ *  Exemplos de dependências para conexão com o BD
+ * 
+ *      Banco de dados relacional:
+ * 
+ *          Sequelize -> Foi utilizado em muitos projetos desde o início 
+ *          do node
+ *          Prisma    -> É uma dependência atual que trabalha com 
+ *          BD (SQL ou ORM)
+ *          Knex      -> É uma depêndencia atual que trabalha com MySQL
+ *      
+ *      Banco de dados não relacional:
+ *          
+ *          Mongoose  -> É uma dependência para o Mongo DB (Não relacional)
+ * 
+ ********************************************************************/
+
+//Import da dependência do prisma que permite a execução do de script SQL no BD 
+const {PrismaClient} = require('@prisma/client') 
+
+//Cria um novo objeto baseado na classe PrismaClient
+const prisma = new PrismaClient()
+
+    //$queryRawUnsafe -> permite  executar scripts SQL de uma
+    //de uma variável e que retorna valores o banco (SELECT)
+
+    //$executeRawUnsafe -> permite executar script SQL de uma
+    //variável e que não retorna dados do banco (INSERT, UPDATE e DELETE)
+
+    //$queryRaw ->  permite  executar scripts SQL de uma
+    //de uma variável e que retorna valores o banco (SELECT)
+    //e executa tratamentos com segurança
+
+    //$executeRaw -> permite executar script SQL de uma
+    //variável e que não retorna dados do banco (INSERT, UPDATE e DELETE)
+    //e executa tratamentos com segurança
+
+// Retorna uma lista de todos os filmes do banco de dados
+const getSelectAllFilms = async () => {
+
+    try {
+       
+        // Script SQL
+        let sql = `select * from tb_filme order by id desc`
+
+        // Encaminha o script para o banco de dados
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        let verify = result.length > 0 ? result : false
+
+        return verify
+    
+    } catch (error) {
+
+        return false
+
+    }
+    
+
+}
+
+// Retorna um filme filtrando pelo id do banco de dados
+const getSelectByIdFilms = async (id) => {
+
+} 
+
+// Insere um filme novo no banco de dados
+const setInsertFilms = async () => {
+
+}
+
+// Altere um filme no banco de dados
+const setUpdateFilms = async () => {
+
+}
+
+// Exclui um filme pelo id no banco de dados
+const setDeleteFilms = async (id) => {
+
+}
+
+module.exports = {
+    getSelectAllFilms
+}
